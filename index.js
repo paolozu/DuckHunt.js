bddea503fe782933794c38faa162d7f72247bf9e
Paolo <adam.paolo@gmail.com>
2020-10-19 21:39:30 +0200

    adds move, positions, shot

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 function Duck() {
+
   const duck = document.createElement('div');
-  
+  duck.classList.add('duck');
+  document.body.appendChild(duck);
+
+  // batti le ali
+  setInterval(() => {
+    duck.classList.toggle('flap');
+  }, (500));
+
+  // genera posizione iniziale papera
+  let medialPosition = Math.random() * window.innerHeight;
+  let lateralPosition = Math.random() * window.innerWidth;
+  duck.style.top = `${medialPosition}px`;
+  duck.style.left = `${lateralPosition}px`;
+
+  setInterval(() => {
+    const newMedialPosition = Math.random() * window.innerHeight;
+    const newLateralPosition = Math.random() * window.innerWidth;
+    // aggiunge e rimuove la classe 'right' a seconda della direzione in cui la papera si sta muovendo.
+    if (lateralPosition < newLateralPosition) {
+      duck.classList.add('right');
+    } else {
+      duck.classList.remove('right');
+    }
+    // update position of duck to new coordinates
+    duck.style.top = `${newMedialPosition}px`;
+    duck.style.left = `${newLateralPosition}px`;
+  }, (500));
+
+  // aggiunge effetto sparo dopo il click
+  duck.addEventListener('click', (event) => {
+    event.target.classList.add('shot');
+    setTimeout(() => {
+      duck.parentNode.removeChild(duck);
+    }, 300);
+  });
 }
+
+window.onload = () => {
+  Duck();
+};
